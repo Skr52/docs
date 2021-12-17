f19da1b1245e3c71855476e852d4e23eccddf650
hzyq0006 <67930242@qq.com>
2021-12-17 15:58:39 +0800

    pret: vuePress.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -8,62 +8,62 @@ module.exports = {
         }]
     ],
     port: 8917,
-    // dest: 'docs',
-    base: '/',
+    base: '/doc/',
+    dest: '.vuepress/doc',
     themeConfig: {
         sidebar: [{
-            title: '导读',
-            collapsable: false,
-            children: ['/home/home']
-        },
-        {
-            title: 'JS',
-            collapsable: false,
-            children: [
-                '/home/JavaScript/knowledge',
-                '/home/JavaScript/TS'
-            ]
-        },
-        {
-            title: 'Vue',
-            collapsable: false,
-            children: ['/home/Vue/standard', '/home/Vue/v3']
-        },
-        {
-            title: 'Git',
-            collapsable: false,
-            children: ['/home/git/git']
-        },
-        {
-            title: '工作记录',
-            collapsable: false,
-            children: ['/home/day-record/record']
-        },
-        {
-            title: '常用网站',
-            collapsable: false,
-            children: ['/home/href/href']
-        },
-        {
-            title: '面试题',
-            collapsable: false,
-            children: [
-                '/home/ms/ms',
-                '/home/ms/desc'
-            ]
-        },
-        {
-            title: '常用方法',
-            collapsable: false,
-            children: [
-                '/home/methods/currentDate',
-                '/home/methods/formatDate',
-                '/home/methods/hidePhone',
-                '/home/methods/unique',
-                '/home/methods/strNull',
-                '/home/methods/lineFeed'
-            ]
-        },
+                title: '导读',
+                collapsable: false,
+                children: ['/home/home']
+            },
+            {
+                title: 'JS',
+                collapsable: false,
+                children: [
+                    '/home/JavaScript/knowledge',
+                    '/home/JavaScript/TS'
+                ]
+            },
+            {
+                title: 'Vue',
+                collapsable: false,
+                children: ['/home/Vue/standard', '/home/Vue/v3']
+            },
+            {
+                title: 'Git',
+                collapsable: false,
+                children: ['/home/git/git']
+            },
+            {
+                title: '工作记录',
+                collapsable: false,
+                children: ['/home/day-record/record']
+            },
+            {
+                title: '常用网站',
+                collapsable: false,
+                children: ['/home/href/href']
+            },
+            {
+                title: '面试题',
+                collapsable: false,
+                children: [
+                    '/home/ms/ms',
+                    '/home/ms/desc'
+                ]
+            },
+            {
+                title: '常用方法',
+                collapsable: false,
+                children: [
+                    '/home/methods/currentDate',
+                    '/home/methods/formatDate',
+                    '/home/methods/hidePhone',
+                    '/home/methods/unique',
+                    '/home/methods/strNull',
+                    '/home/methods/lineFeed'
+                ]
+            },
         ],
         sidebarDepth: 2,
         lastUpdated: '更新时间'
